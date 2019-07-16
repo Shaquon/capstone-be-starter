55e48e30c2e48939d68630ad771abd026041525a
Shaquon <shaquonkelley@gmail.com>
2019-07-16 14:59:03 -0400

    adding roles

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    role: { type: String, required: true} 
   },
   {
     timestamps: true,
