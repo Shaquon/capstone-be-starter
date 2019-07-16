9487b843b48642d606f352d5dd92a9557ed7bd53
Shaquon <shaquonkelley@gmail.com>
2019-07-16 13:02:12 -0400

    working on login

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,31 @@
 const { User } = require("../models");
+const { validateJwtMiddleware } = require("../auth");
+
+function getRawUser(user) {
+  const rawUser = user.toJSON();
+  delete rawUser.password;
+  delete rawUser.picture;
+  delete rawUser.pictureContentType;
+  return rawUser;
+}
+
+const getUser = async (req, res, next) => {
+  const id = req.params.username;
+  try {
+    const user = await User.scope(null).findById(id);
+
+    if (!user) {
+      next({
+        statusCode: 404,
+        message: "User does not exist"
+      });
+      return;
+    }
+    res.send({ user: getRawUser(user), statusCode: res.statusCode });
+  } catch (err) {
+    next(err);
+  }
+};
 
 // create a new user
 const createUser = async (req, res, next) => {
@@ -8,12 +35,13 @@ const createUser = async (req, res, next) => {
       username,
       password
     });
-    res.send({ username: user.get("username"), statusCode: res.statusCode });
+    res.send({ user: getRawUser(user), statusCode: res.statusCode });
   } catch (err) {
     next(err);
   }
 };
 
 module.exports = {
-  createUser
-};
+  createUser,
+  getUser
+}
